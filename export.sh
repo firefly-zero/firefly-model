a98669f023f81e085896c6a5cfe6b96a4be20682
gram <git@orsinium.dev>
2026-01-18 10:48:31 +0100

    export script

---
+++ export.sh
@@ -0,0 +1,33 @@
+mkdir -p stl
+echo "back"
+openscad -o stl/back.stl                models/back.scad
+echo "battery"
+openscad -o stl/battery.stl             models/battery.scad
+echo "bend"
+openscad -o stl/bend.stl                models/bend.svg
+echo "button-holder"
+openscad -o stl/button-holder.stl       models/button-holder.scad
+echo "button"
+openscad -o stl/button.stl              models/button.scad
+echo "display"
+openscad -o stl/display.stl             models/display.scad
+echo "front"
+openscad -o stl/front.stl               models/front.scad
+echo "logo"
+openscad -o stl/logo.stl                models/logo.scad
+echo "logo"
+openscad -o stl/logo.stl                models/logo.svg
+echo "pcb"
+openscad -o stl/pcb.stl                 models/pcb.scad
+echo "pcb"
+openscad -o stl/pcb.stl                 models/pcb.stl
+echo "power-button"
+openscad -o stl/power-button.stl        models/power-button.scad
+echo "simplified-button"
+openscad -o stl/simplified-button.stl   models/simplified-button.scad
+echo "speaker"
+openscad -o stl/speaker.stl             models/speaker.scad
+echo "touchpad"
+openscad -o stl/touchpad.stl            models/touchpad.scad
+echo "translucy"
+openscad -o stl/translucy.stl           models/translucy.scad
